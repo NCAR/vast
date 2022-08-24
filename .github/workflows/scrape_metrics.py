ca9ce4aad42ef30fc0ed9075d3af85e35c120172
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-08-24 10:39:44 -0600

    par down scrape_metrics and dump to metrics

--- .github/workflows/scrape_metrics.py
+++ .github/workflows/scrape_metrics.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import argparse
 from binstar_client.utils import get_server_api
 import binstar_client.errors
@@ -6,6 +5,7 @@ import binstar_client.errors
 import os
 import sqlite3
 import sys
+import yaml
 
 # detect script dir
 abspath = os.path.abspath(__file__)
@@ -24,51 +24,37 @@ args = parser.parse_args()
 conn = sqlite3.connect(args.db_file)
 c = conn.cursor()
 
-packages = ['geocat-comp', 'geocat-f2py', 'geocat-datafiles', 'geocat-viz', 'ncl', 'pynio', 'pyngl', 'uxarray', 'wrf-python']
+packages_file = 'data/packages.yml'
+with open(packages_file) as f:
+  packages_dict = yaml.load(f, Loader=yaml.CLoader)
+packages = [i['package-name'] for i in packages_dict['packages']]
 conda_channels = ['conda-forge', 'ncar']
 
 if args.packages:
     packages = args.packages
 
+download_list = []
 for package in packages:
     package_total_dls = 0
     for conda_channel in conda_channels:
-        #db_table = "%s_%s" % (package, conda_channel)
         db_table = package.replace('-', '')
 
         aserver_api = get_server_api(log_level=0)
         try: package_obj = aserver_api.package(conda_channel, package)
         except binstar_client.errors.NotFound: continue
-        try:
-            c.execute('''CREATE TABLE %s (channel text, version text, build text, platform text, arch text, pyversion text, npversion text, uploaded_date date, downloads int, check_date datetime default CURRENT_TIMESTAMP)''' % db_table)
-            conn.commit()
-        except sqlite3.OperationalError:
-            pass
+   
         for version_str in package_obj['versions']:
             version = aserver_api.release(conda_channel, package, version_str)
             for d in version['distributions']:
-                #distribution_os = d['attrs']['operatingsystem']
-                distribution_os = d['attrs']['platform']
-                #distribution_arch = d['attrs']['arch']
-                distribution_arch = d['attrs']['machine']
-                distribution_build = d['attrs']['build']
-                distribution_downloads = d['ndownloads']
-                distribution_upload_time = d['upload_time']
+                distribution_downloads = d['ndownloads']\
+                
+                download_list.append(distribution_downloads)
 
-                # detect numpy and python versions
-                np_version = None
-                py_version = None
-                for item in d['dependencies']['depends']:
-                    try:
-                        if item['name'] == 'numpy':
-                            np_version = item['specs'][0][1]
-                        if item['name'] == 'python':
-                            py_version = item['specs'][0][1]
-                    except: pass
-                #for key in d.keys():
-                #    print "%s: %s" % (key, d[key])
-                #sys.exit(0)
-                c.execute('''INSERT INTO %s (channel, version, build, platform, arch, pyversion, npversion, uploaded_date, downloads) VALUES (?,?,?,?,?,?,?,?,?)''' % db_table,
-                             (conda_channel, version_str, distribution_build, distribution_os, distribution_arch, py_version, np_version, distribution_upload_time, distribution_downloads))
-                conn.commit()
-conn.close()
+downloads = sum(download_list)
+
+metrics_file = 'data/metrics.yml'
+with open(metrics_file, 'r+') as f:
+  metrics_dict = yaml.load(f, Loader=yaml.CLoader)
+metrics_dict['counter_item'][0]["number"] = downloads
+with open(metrics_file, 'w') as f:
+  yaml.dump(metrics_dict, f)

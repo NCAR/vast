d3f8b4959334dc2ea3f07d3f5c75a40bec49069f
David John Gagne <davidjohngagne@gmail.com>
2024-08-06 09:45:42 -0500

    Update current projects and other stuff

--- content/current_projects/_index.md
+++ content/current_projects/_index.md
@@ -3,26 +3,8 @@ title : "Current Projects"
 date  : 2022-04-27T00:00:00-06:00
 image : images/banners/cloudSimulation.png
 ---
-VAST provides visualization services to researchers who need assistance with visualizing data to demonstrate the results of their scientific computing. This service is available to researchers who want help using specialized applications on data analysis and visualization resources that CISL manages.
-
-VAST visualization staff have particular expertise in CISL-developed software such as VAPOR and GeoCAT but are open to assisting with other applications. They have helped science teams produce numerous visualizations for conferences, publications, and scientific journals as well as science and broadcast news programs.
-
-Visit the [CISL Visualization Gallery](https://visgallery.ucar.edu/) for examples.
-
-##### Contact us: [Visualization services request form](https://forms.gle/9Z16PVfnQLUFauvBA)
-
-#### Additional links
-
-[ESDS Office Hours](https://sundog.ucar.edu/Interact/Pages/Content/Document.aspx?id=6160)
-
-[![NCAR VisLab YouTube Channel](../images/current_projects/youtube.png)](https://www.youtube.com/user/ucarvets/videos)
 
 
 
 
 
-<!-- [<img src= ../images/current_projects/youtube.png>](http://google.com.au/) -->
-
-<!-- ![Visualization of hurricane Odile](../images/current_projects/odile.jpg) -->
-
-<!-- [<img src="../images/current_projects/odile.jpg" alt="Visualization of hurricane Odile" style="width:48%">] -->
---
+++ themes/ncar/layouts/partials/stormdata.js
@@ -0,0 +1,116 @@
+
+async function read_valid_dates(valid_init_hours=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23])
+{
+    let date_filename = "https://storm-mode.s3.amazonaws.com/model_output/available_dates.csv";
+    let date_resp = await fetch(date_filename);
+    let date_str = await date_resp.text();
+    let run_date_strs = date_str.split("\n");
+    let dates = [];
+    for (let i=0; i< run_date_strs.length-1; i++) {
+        dates.push(new Date(Date.UTC(parseInt(run_date_strs[i].substring(0, 4)),
+                            parseInt(run_date_strs[i].substring(4, 6)) -1,
+                            parseInt(run_date_strs[i].substring(6, 8)),
+                            parseInt(run_date_strs[i].substring(8, 10)),
+                            parseInt(run_date_strs[i].substring(10, 12)))
+        ));
+    }
+    dates = dates.filter(function(d) {return valid_init_hours.includes(d.getUTCHours());});
+    dates = dates.reverse();
+    return dates;
+}
+
+function date_range(start_date_str, end_date_str, date_diff_hours=24) {
+    let start_date = new Date(start_date_str);
+    let end_date = new Date(end_date_str);
+    let hours_to_ms = 3600000;
+    let curr_date = new Date(start_date);
+    let dates = [];
+    while (curr_date <= end_date)
+    {
+        dates.push(curr_date);
+        curr_date = new Date(curr_date.getTime() + date_diff_hours * hours_to_ms);
+    }
+    dates = dates.reverse();
+    return dates;
+}
+
+function hrrr_json_date_str(date_str) {
+    let init_time = new Date(date_str);
+    let init_date_str = String(init_time.getUTCFullYear());
+    init_date_str += String(init_time.getUTCMonth() + 1).padStart(2, "0");
+    init_date_str += String(init_time.getUTCDate()).padStart(2, "0");
+    init_date_str += "-" + String(init_time.getUTCHours()).padStart(2, "0") + "00";
+    return init_date_str;
+}
+
+function hrrr_csv_date_str(date_str) {
+    let init_time = new Date(date_str);
+    let init_date_str = String(init_time.getUTCFullYear()) + "-";
+    init_date_str += String(init_time.getUTCMonth() + 1).padStart(2, "0") + "-";
+    init_date_str += String(init_time.getUTCDate()).padStart(2, "0");
+    init_date_str += "_" + String(init_time.getUTCHours()).padStart(2, "0") + "00";
+    return init_date_str;
+}
+
+async function read_csv(filename) {
+    let data = {};
+    let str_cols = ["Valid_Date", "Run_Date", "Step_ID", "Track_ID", "SS_label", "CNN_1_label", "DNN_1_label"];
+    let int_cols = ["Forecast_Hour", "Duration"];
+    await d3.csv(filename, function(row) {
+        if (Object.keys(data).length === 0) {
+            console.log(row);
+            Object.keys(row).forEach(function (col) {
+               data[col] = [];
+            });
+        }
+        Object.keys(row).forEach(function (col) {
+            if (str_cols.includes(col)) {
+                data[col].push(row[col]);
+            }
+            else if (int_cols.includes(col)) {
+                data[col].push(parseInt(row[col]));
+            }
+            else {
+                data[col].push(parseFloat(row[col]));
+            }
+            });
+    });
+    return data;
+}
+
+function filter_time(valid_time, time_arr, index_arr, data_arr) {
+    let selected_data = {};
+    selected_data["index"] = index_arr.filter(function (i) {return time_arr[i] === valid_time;});
+    Object.keys(data_arr).forEach(function(col) {selected_data[col] = [];});
+    selected_data["index"].forEach(function (t)  {
+        Object.keys(data_arr).forEach(function(col) {selected_data[col].push(data_arr[col][t]);});
+    });
+    return selected_data;
+}
+
+function filter_time_and_mode(valid_time, mode, time_arr, mode_arr, index_arr, data_arr) {
+    let selected_data = {};
+    selected_data["index"] = index_arr.filter(function (i) {return (time_arr[i] === valid_time) && (mode_arr[i] === mode);});
+    selected_data["hovertext"] = [];
+    hover_cols = [["CNN_1_Supercell_prob", "CNN_1_QLCS_prob","CNN_1_Disorganized_prob"],
+                 ["DNN_1_Supercell_prob", "DNN_1_QLCS_prob", "DNN_1_Disorganized_prob"],
+                 ["SS_Supercell_prob", "SS_QLCS_prob", "SS_Disorganized_prob"]];
+    Object.keys(data_arr).forEach(function(col) {selected_data[col] = [];});
+    selected_data["index"].forEach(function (t)  {
+        Object.keys(data_arr).forEach(function(col) {selected_data[col].push(data_arr[col][t]);});
+        let hover_str = "S: " + data_arr[sel_model + "_Supercell_prob"][t].toFixed(2) + "<br>";
+        hover_str += "L: " + data_arr[sel_model + "_QLCS_prob"][t].toFixed(2) + "<br>";
+        hover_str += "D: "  + data_arr[sel_model + "_Disorganized_prob"][t].toFixed(2) + "<br>";
+        hover_str += "UH: " + data_arr["MXUPHL_1hr_max_fcst-5000_2000m_above_ground_max"][t].toFixed(0) + "<br>";
+        //hover_str += "CAPE: " + data_arr["CAPE-0_3000m_above_ground-potential_max"][t].toFixed(0) + " ";
+        let shear_mag = (data_arr["VUCSH-0_6000m_above_ground-potential_mean"][t] ** 2 +
+            data_arr["VVCSH-0_6000m_above_ground-potential_mean"][t] ** 2) ** 0.5;
+        hover_str += "Shear: " + shear_mag.toFixed(0) + " ";
+
+
+        selected_data["hovertext"].push(hover_str);
+
+    });
+
+    return selected_data;
+}
